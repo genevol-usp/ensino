8b6711032b74261ce64c9cea77d1271379a783ad
Diogo Meyer <diogo@ib.usp.br>
2017-05-18 09:14:09 -0300

    Fórmula em português e ajustes de indenteção

--- popgen_functions.R
+++ popgen_functions.R
@@ -1,20 +1,24 @@
-# Basic selection model with w 
+# Modelo básico de seleção parametrizado com w 
+
 delta.p.old <- function(p, w11, w12, w22)
-        {
-                    w.bar   <- p^2*w11 + 2*p*(1-p)*w12 + (1-p)^2*w22
-        p.prime <- (p^2*w11 + p*(1-p)*w12)/w.bar
-              return(p.prime)
-            }
+  {
+    w.bar   <- p^2*w11 + 2*p*(1-p)*w12 + (1-p)^2*w22
+    p.prime <- (p^2*w11 + p*(1-p)*w12)/w.bar
+    return(p.prime)
+  }
 
 
-# Basic selection function, parametrized with h and s
+# Modelo básico de seleção parametrizado com w e h
+
 delta.p <- function(p, s, h)
-       {
-        w11    <- 1
-        w12    <- 1 - h * s
-        w22    <- 1 - s
-        w.bar  <- p^2*w11 + 2*p*(1-p)*w12 + (1-p)^2*w22
-        
-        p.prime <- (p^2*w11 + p*(1-p)*w12)/w.bar
-        return(p.prime)
-       }
+
+{
+    w11    <- 1
+    w12    <- 1 - h * s
+    w22    <- 1 - s
+
+    w.bar  <- p^2*w11 + 2*p*(1-p)*w12 + (1-p)^2*w22
+    p.prime <- (p^2*w11 + p*(1-p)*w12)/w.bar
+    
+    return(p.prime)
+  }
