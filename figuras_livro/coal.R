16517a351262d27586cc3744520141a9498f48b4
Diogo Meyer <diogo@farofa.local>
2017-06-14 08:37:27 -0300

    Code for coalescent figure

--- figuras_livro/coal.R
+++ figuras_livro/coal.R
@@ -3,8 +3,14 @@
 # Probability of coalescent of 2 lineages in generation t
 # N diploid individuals, sample of n=2
 
-pr.coal2.t <- function(N,t){
-
-x <- ((1/(2*N))^(t-1)) * (1/(2*N))
+pr.coal2.t <- function(N,t) {
+x <- 1/(2*N) * (1 - 1/(2*N))^(t-1)
 return(x)
 }
+ 
+t <- seq(1:100)
+
+result <- pr.coal2.t(10,t)
+
+plot(result ~ t, ylab = "Tempo de coalescência")
+result
