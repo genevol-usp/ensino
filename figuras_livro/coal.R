6d9392a6ad5c8bd9f50c403eff78aeeebaf10e46
Diogo Meyer <diogo@farofa.local>
2017-06-14 09:31:21 -0300

    coal and ibd done

--- figuras_livro/coal.R
+++ figuras_livro/coal.R
@@ -7,11 +7,31 @@ pr.coal2.t <- function(N,t) {
 x <- 1/(2*N) * (1 - 1/(2*N))^(t-1)
 return(x)
 }
+
+
+pr.ibd <- function(N,t) {
+x <- 1 - ( 1- (1/(2*N)))^t  
+}
+
  
 t <- seq(1:100)
 
-result <- pr.coal2.t(10,t)
+pcoal <-pr.coal2.t(10,t)
+ibd <- pr.ibd(10,t)
 
-plot(result ~ t, ylab = "Tempo de coalescência")
-result
-# comment
+
+result <- as.data.frame(cbind(t,pcoal))
+result$cumulative <- cumsum(result$pcoal)
+result$ibd <- pr.ibd(10,t)
+
+
+par(mfrow=c(2,2))
+plot(result$pcoal ~ result$t, ylab = "Tempo de coalescência", type="l")
+plot(result$cumulative ~ result$t, ylab = "Tempo de coalescência", type="l")
+plot(result$ibd ~ result$t, ylab = "IBD", type="l")
+
+library(ggplot2)
+
+#ggplot(data = result, aes(y=pcoal, x=t))+geom_line()
+#ggplot(data = result, aes(y=cumulative, x=t))+geom_line()
+
